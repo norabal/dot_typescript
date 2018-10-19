1f0a22698263718447b0c06bad4e9d1e4c3d93cb
norabal <norabal.works@gmail.com>
2018-10-19 23:21:10 +0900

    #02

---
+++ main.js
@@ -0,0 +1,6 @@
+var User = /** @class */ (function () {
+    function User() {
+    }
+    return User;
+}());
+console.log("hello world");
---
+++ main.ts
@@ -0,0 +1,5 @@
+class User {
+
+}
+
+console.log("hello world");

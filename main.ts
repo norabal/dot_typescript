46527b4038aa629e66f174a6489086c543cfd9fc
norabal <norabal.works@gmail.com>
2018-10-20 21:34:56 +0900

    #18 内部モジュール tsc main.ts --out all.js node all.js で動く

--- main.ts
+++ main.ts
@@ -1,29 +1,6 @@
-// Generics
+/// <reference path="./user.ts" />
 
-interface Result {
-    a: number;
-    b: number;
-}
-interface FinalResult {
-    a: number;
-    b: number;
-    c: string;
-}
+console.log(UserModule.name);
 
-class MyData<T extends Result> {
-    constructor(public value: T) {
-    }
-
-    getArray(): T[] {
-        return [this.value, this.value, this.value];
-    }
-}
-// var v1 = new MyData<string>("hello");
-// console.log(v1.getArray());
-// var v2 = new MyData<number>(234);
-// console.log(v2.getArray());
-
-// var v3 = new MyData<Result>({a: 32, b: 16});
-// console.log(v3.getArray());
-var v4 = new MyData<FinalResult>({a: 32, b: 16, c: "hello"});
-console.log(v4.getArray());
+import addr = UserModule.AddressModule;
+console.log(addr.zip);
---
+++ user.ts
@@ -0,0 +1,6 @@
+module UserModule {
+    export var name = "norabal";
+    export module AddressModule {
+        export var zip = "111-1111";
+    }
+}

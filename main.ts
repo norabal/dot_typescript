797fdba129ab03d09f264dd381066060fbaf57a1
norabal <norabal.works@gmail.com>
2018-10-20 20:59:23 +0900

    #17 ジェネリクスに制限

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 // Generics
 
-function getArray<T>(value: T): T[] {
-    return [value, value, value];
+interface Result {
+    a: number;
+    b: number;
+}
+interface FinalResult {
+    a: number;
+    b: number;
+    c: string;
 }
 
-console.log(getArray<number>(3));
-console.log(getArray<string>("hello"));
+class MyData<T extends Result> {
+    constructor(public value: T) {
+    }
+
+    getArray(): T[] {
+        return [this.value, this.value, this.value];
+    }
+}
+// var v1 = new MyData<string>("hello");
+// console.log(v1.getArray());
+// var v2 = new MyData<number>(234);
+// console.log(v2.getArray());
+
+// var v3 = new MyData<Result>({a: 32, b: 16});
+// console.log(v3.getArray());
+var v4 = new MyData<FinalResult>({a: 32, b: 16, c: "hello"});
+console.log(v4.getArray());
